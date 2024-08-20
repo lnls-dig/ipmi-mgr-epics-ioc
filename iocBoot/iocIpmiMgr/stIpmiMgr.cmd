1288d0233b84a36053c5b044766bd47042180555
Gustavo de Souza dos Reis <gustavo.reis@lnls.br>
2024-08-20 16:05:25 -0300

    Install asynRecord locally.
    After build, epics_in_docker deletes the directories with the support
    modules. For this reason, we need to install locally all support module
    artifacts that are used by the IOC at runtime.

--- iocBoot/iocIpmiMgr/stIpmiMgr.cmd
+++ iocBoot/iocIpmiMgr/stIpmiMgr.cmd
@@ -15,7 +15,7 @@ drvAsynIPPortConfigure ("$(PORT)","$(IPADDR):623 udp",0,0,0)
 mchInit("$(PORT)")
 
 ## Load record instances
-dbLoadRecords("$(ASYN)/db/asynRecord.db","P=${P}, R=${R}:asyn,PORT=$(PORT),ADDR=0,OMAX=80,IMAX=80")
+dbLoadRecords("$(TOP)/db/asynRecord.db","P=${P}, R=${R}:asyn,PORT=$(PORT),ADDR=0,OMAX=80,IMAX=80")
 dbLoadRecords("${TOP}/db/microtca_bpm_crate.db", "link=$(PORT),P=$(P),R=$(R):,dev=$(P)$(R),unit='',aliasprefix=''")
 
 # < save_restore.cmd
